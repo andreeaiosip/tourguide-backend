e20f44790c3f739e6de0c076874588a9dc80fbba
Andreea Iosip <andrea.iosip@gmail.com>
2020-04-05 20:50:23 +0100

    Delete and Add tourguides function works

--- admin/tourguides.php
+++ admin/tourguides.php
@@ -1,14 +1,15 @@
 <?php
-$fName 		= $_REQUEST["guideName"];
-$lName 		= $_REQUEST["guideSurname"];
-$commLevel  = $_REQUEST["commLevel"];
-$active 	= $_REQUEST["active"];
-
+$guideName		= $_REQUEST["guideName"];
+$guideSurname 	= $_REQUEST["guideSurname"];
+$commLevel  	= $_REQUEST["commLevel"];
+$active 		= $_REQUEST["active"];
+$guideUid		= $_REQUEST["guideUid"];
 
 
 $procedure = $_REQUEST["procedure"] ;
 include('myDBConnection.php');
 include('commonFunctions.php');
+include('deleteFunction.php');
 
 
 
@@ -55,39 +56,87 @@ include('commonFunctions.php');
 			   echo '<td>'.ucwords($Arrayline['TourName']).'</td>';
 			   echo '<td>'.$Arrayline['Price'].'</td>';
 			   echo '<td>';
-			   // the guideuid is for PHP
-			   // the uid is from the record set and im getting that from the DB!
-			   echo '<a href="home.php?procedure=guides_deleteme&guideuid='.$Arrayline['uid'].'" title="This will delete me"><i class="fa fa-trash fa fa-2x" aria-hidden="true"></i></a>';
-			   echo '<a href="home.php?procedure=guides_editme&guideuid='.$Arrayline['uid'].'" title="This will Edit me"><i class="fa fa-pencil-square fa-2x" aria-hidden="true"></i></a>';
+			   echo '<a href="tourguides.php?procedure=deleteTourGuide&guideUid='.$Arrayline['uid'].'" title="This will delete me"><i class="fa fa-trash fa fa-2x" aria-hidden="true"></i></a>';
+			   echo '<a href="tourguides.php?procedure=guides_editme&guideUid='.$Arrayline['uid'].'" title="This will Edit me"><i class="fa fa-pencil-square fa-2x" aria-hidden="true"></i></a>';
 			   echo '</td>';
 			 echo '</tr>';
 		}
-	
 	  echo '</table>';
 	
-	//  echo '<button class="btn success">Add Guide</button>'; / started with this...
-	  echo '<button class="btn success"><a href="home.php?procedure=addnewguide"><i class="fa fa-user-circle-o" aria-hidden="true"></i> Add Guide</a></button>';
-	
-	
-	 }
- 
- if ($procedure=="deleteme"){
- 
-	 $uid 	   = $_REQUEST["uid"] ;
-	 $query    = "DELETE FROM tourGuide WHERE uid='".$uid."'";
-	 $result   = mysqli_query($dbConn, $query);
- 
-	 echo '<meta http-equiv="refresh" content="0;url=home.php?procedure=tourguides" />';
- 
-	 }
- 
-	/* if ($procedure=="guide_deleteme"){
 
-		$guideuid = $_REQUEST["guideUid"];
-		// connecttion, tourgide table, the Record i want to remove, which page after i have deleted
-		Delete_A_record($dbConn,"tourguide",$guideUid,"guides");
+	  echo '<button class="btn success"><a href="tourguides.php?procedure=addnewtourguide"><i class="fa fa-user-circle-o" aria-hidden="true"></i> Add Guide</a></button>';
 	
-		}*/
+	
+	 }
+	 if ($procedure=="deleteTourGuide"){
+
+	
+		Delete_A_record($dbConn,"tourGuide",$guideUid);
+	
+		echo '<meta http-equiv="refresh" content="0;url=tourguides.php?procedure=tourguides&showSnack=Record Deleted! successfully..." />';
+	
+		}
+
+
+ 
+// ADD NEW TOUR------------------
+ 
+ 
+if ($procedure=="addnewtourguide"){
+ 
+	$wasiposted 	= $_REQUEST["wasiposted"] ;
+	
+	$myerror = "Please complete the below!";
+	
+	
+	if ($wasiposted<> ""){
+	
+		if (strlen($Desc) < 15){
+	
+		  $myerror .= "<br><br><br><BIG><BIG><BIG>Please enter a more comprehensive description</BIG></BIG></BIG>";
+	
+			}
+	
+	}
+	
+	echo $myerror;
+	
+	
+	
+	echo '
+	  <form action="tourguides.php">
+	  <input type="hidden" name="procedure" value= '.$procedure.'>
+	  <label for="guideName">Guide Name:</label><br>
+	  <input type="text" id="guideName" name="guideName" required value="'.$guideSurname.'"><br>
+	  <label for="guideSurname">Guide Surname:</label><br>
+	  <input type="text" id="guideSurname" size="30" name="guideSurname" required value="'.$guideSurname.'"><br><br>
+	  <label for="commLevel">Comm Level:</label><br>
+	  <input type="text" id="commLevel" name="commLevel" required value="'.$commLevel.'"><br><br>
+	  <input type="hidden" name="wasiposted" value="formposted">
+	  <input type="submit" value="Submit">
+	  </form>';
+	
+	
+	if ($wasiposted <> ""){
+	
+		 $query = "INSERT INTO tourGuide (guideName, guideSurname, commLevel) VALUES (".
+					"'".$guideName."',".
+					"'".$guideSurname."',".
+					"'".$commLevel."')";
+	
+	   
+		$result = mysqli_query($dbConn, $query);
+	
+	
+		echo "Record added successfully...";
+	
+		echo '<meta http-equiv="refresh" content="0;url=tourguides.php?procedure=tourguides" />';
+	
+		}
+	
+	 }
+
+ 
  
  if ($procedure=="editme"){
  
@@ -100,16 +149,16 @@ include('commonFunctions.php');
 		 $query = "SELECT * FROM tourGuide WHERE uid='".$uid."'";
 		 $result = mysqli_query($dbConn, $query);
 		 while($Arrayline = mysqli_fetch_assoc($result)) {
-			 $fName 	= $Arrayline["guideName"];
-			 $lName 	= $Arrayline["guideSurname"];
+			 $guideName 	= $Arrayline["guideName"];
+			 $guideSurname 	= $Arrayline["guideSurname"];
              $commLevel = $Arrayline["commLevel"];
              $active 	= $Arrayline["active"];
 			 }
  
 		 }else{
  
-			 $fName 	= $_REQUEST["guideName"];
-			 $lName 	= $_REQUEST["guideSurname"];
+			 $guideName 	= $_REQUEST["guideName"];
+			 $guideSurname	= $_REQUEST["guideSurname"];
              $commLevel = $_REQUEST["commLevel"];
              $active	= $_REQUEST["active"];
 			 }
@@ -117,7 +166,7 @@ include('commonFunctions.php');
 
  if ($myerror=="" && $fromDB <> ""){
  
-	  $query = "UPDATE tourGuide SET guideName='".addslashes($fName)."',guideSurname='".addslashes($lName)."'commLevel='".addslashes($commLevel)."',active='".$active."' WHERE uid='".$uid."'";
+	  $query = "UPDATE tourGuide SET guideName='".addslashes($guideName)."',guideSurname='".addslashes($guideSurname)."'commLevel='".addslashes($commLevel)."',active='".$active."' WHERE uid='".$uid."'";
 	 $result = mysqli_query($dbConn, $query);
  
 	 echo "Record updated successfully...";
