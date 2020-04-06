c0cbf35ad537cc8add1bcd23d8c0a8b83a4261de
Andreea Iosip <andrea.iosip@gmail.com>
2020-04-06 12:18:33 +0100

    Commission level updated

--- admin/commission.php
+++ admin/commission.php
@@ -1,12 +1,14 @@
 <?php
 
-  $procedure = $_REQUEST["procedure"] ;
+$commDescription = $_REQUEST["commDescription"] ;
+$commPercent 	 = $_REQUEST["commPercent"] ;
+$wasiposted 	 = $_REQUEST["wasiposted"] ;
+$cuid 	  		 = $_REQUEST["cuid"] ;
+$fromDB 		 = $_REQUEST["fromDB"] ;
+$procedure		 = $_REQUEST["procedure"] ;
 
   include('myDBConnection.php');
-
-// down below deck i called !
-//Delete_A_record($dbConn,"tours",$tuid,"tours");
-
+  include('deleteFunction.php');
   include('commonFunctions.php');
 
 ?>
@@ -45,7 +47,7 @@ if ($procedure=="commission"){
 echo '<table>
   <tr>
     <th>Commission ID</th>
-    <th>Comms Description</th>
+    <th>Comms Level</th>
     <th>Commission Rate</th>
     <th>Action</th>
   </tr>';
@@ -58,7 +60,7 @@ echo '<table>
 	     echo '<td>';
 		     echo $Arrayline['uid'];
 		 echo '</td>';
-		 echo '<td>'.ucwords($Arrayline['description']).'</td>';
+		 echo '<td>'.ucwords($Arrayline['commDescription']).'</td>';
 
 		 if ($Arrayline['commPercent'] > 0 ) {
 			 echo '<td>'.$Arrayline['commPercent'].'% </td>';
@@ -68,8 +70,8 @@ echo '<table>
 
 
 		 echo '<td>';
-		  echo '<a href="commission.php?procedure=comms_deleteme&cuid='.$Arrayline['uid'].'" title="This will delete me"><i class="fa fa-trash fa fa-2x" aria-hidden="true"></i></a>';
-		  echo '<a href="commission.php?procedure=comms_editme&cuid='.$Arrayline['uid'].'" title="This will Edit me"><i class="fa fa-pencil-square fa-2x" aria-hidden="true"></i></a>';
+		  echo '<a href="commission.php?procedure=deleteCommLevel&cuid='.$Arrayline['uid'].'" title="Delete me"><i class="fa fa-trash fa fa-2x" aria-hidden="true"></i></a>';
+		  echo '<a href="commission.php?procedure=editCommLevel&cuid='.$Arrayline['uid'].'" title="Edit me"><i class="fa fa-pencil-square fa-2x" aria-hidden="true"></i></a>';
 		 echo '</td>';
 		echo '</tr>';
 
@@ -80,19 +82,17 @@ echo '<table>
   echo '</table>';
 
 
-    //echo '<a href="commission.php?procedure=addnewcommlevel"><i class="fa fa-fw fa-plus fa-2x"></i>Add Commission Level</a>';
-    echo '<button class="btn success"><a href="commission.php?procedure=addnewcommlevel"><i class="fa fa-handshake-o fa-lg" aria-hidden="true"></i> Add Commission Level</a></button>';
+    //echo '<a href="commission.php?procedure=addCommLevel"><i class="fa fa-fw fa-plus fa-2x"></i>Add Commission Level</a>';
+    //echo '<button class="btn success"><a href="commission.php?procedure=editCommLevel"><i class="fa fa-handshake-o fa-lg" aria-hidden="true"></i> Edit Commission Level</a></button>';
 
 
 	}
 
 
 
-if ($procedure=="addnewcommlevel"){
+if ($procedure=="addCommLevel"){
 
-$Commname 		= $_REQUEST["Commname"] ;
-$cpercentage 	= $_REQUEST["cpercentage"] ;
-$wasiposted 	= $_REQUEST["wasiposted"] ;
+
 
 $myerror = "Please complete the below!";
 
@@ -107,7 +107,7 @@ if ($wasiposted <> ""){
 
         }
         
-        if ($cpercentage < 1){
+        if ($commPercent < 1){
 
             $myerror .= "<br><br><br><BIG><BIG><BIG>Commission can NOT be Less than 1% of the tour price</BIG></BIG></BIG>";
       
@@ -120,11 +120,11 @@ echo $myerror;
 
 
 echo '
-  <form action="commission.php?procedure=addnewcommlevel" method="post">
+  <form action="commission.php?procedure=addCommLevel" method="post">
   <label for="fname">Commission Level </label><br>
-  <input type="text" id="Commname" name="Commname" required value="'.$Commname.'"><br>
+  <input type="text" id="commDescription" name="commDescription" required value="'.$commDescription.'"><br>
   <label for="lname">Commission Percentage:</label><br>
-  <input type="number" id="cpercentage" name="cpercentage" required value="'.$cpercentage.'"><br><br>
+  <input type="number" id="commPercent" name="commPercent" required value="'.$commPercent.'"><br><br>
   <input type="hidden" name="wasiposted" value="formposted">
   <input type="submit" value="Submit">
   </form>';
@@ -140,14 +140,14 @@ if ($myerror=="" && $wasiposted <> ""){
    // debugging info
    //echo "Fourth<HR>";
 
-    $myNewUid = generateUid();
+    $newCommUid = generateUid();
 
     // added the `` around value and im worried it a reserved word in SQL, i dont take chances with such words.rather
     // safe then a SQL error
-     $query = "INSERT INTO commLevel (uid,description,commPercent) VALUES (".
-                "'".$myNewGuid."',".     
-				"'".$Commname."',".
-				"'".$cpercentage."')";
+     $query = "INSERT INTO commLevel (uid,commDescription,commPercent) VALUES (".
+                "'".$newCommUid."',".     
+				"'".$commDescription."',".
+				"'".$commPercent."')";
 
 	$result = mysqli_query($dbConn, $query);
 
@@ -166,11 +166,9 @@ if ($myerror=="" && $wasiposted <> ""){
  }
 
 
-if ($procedure=="comms_editme"){
+if ($procedure=="editCommLevel"){
 
-	$cuid 	  	= $_REQUEST["cuid"] ;
-	$fromDB 	= $_REQUEST["fromDB"] ;
-    $wasiposted = $_REQUEST["wasiposted"] ;
+	
 	$myerror    = ""; // use this for any validation we need!
 
     // debugging information
@@ -182,13 +180,13 @@ if ($procedure=="comms_editme"){
 		$query = "SELECT * FROM commLevel WHERE uid='".$cuid."'";
 		$result = mysqli_query($dbConn, $query);
 		while($Arrayline = mysqli_fetch_assoc($result)) {
-		    $Commname 	 = $Arrayline["description"];
-	        $cpercentage = $Arrayline["value"];
+		    $commDescription 	 = $Arrayline["commDescription"];
+	        $commPercent = $Arrayline["value"];
 			}
 		}else{
 
-		    $Commname    = $_REQUEST["Commname"];
-	        $cpercentage = $_REQUEST["cpercentage"];
+		    $commDescription    = $_REQUEST["commDescription"];
+	        $commPercent = $_REQUEST["commPercent"];
 
 			}
 
@@ -199,14 +197,14 @@ if ($wasiposted <> ""){
 
 	$myerror ="";
 
-	if ($cpercentage > 50){
+	if ($commPercent > 50){
 
 	  $myerror .= "<br><BIG><BIG>Commission can NOT be bigger then 50% of the tour price</BIG></BIG></BIG>";
 
 		}
 
 
-	if ($cpercentage == 0){
+	if ($commPercent == 0){
 
 	  $myerror .= "<br><BIG><BIG>Commission must be more than Zero!!!</BIG></BIG></BIG>";
 
@@ -219,11 +217,11 @@ echo $myerror." - Hey maybe add some styling here... to make it a little more pr
 
 
 echo '
-  <form action="commission.php?procedure=comms_editme&cuid='.$cuid.'" method="post">
-  <label for="Commname">Commission Level </label><br>
-  <input type="text" id="Commname" name="Commname" required value="'.$Commname.'"><br>
-  <label for="cpercentage">Commission Percentage:</label><br>
-  <input type="number" id="cpercentage" name="cpercentage" required value="'.$cpercentage.'"><br><br>
+  <form action="commission.php?procedure=editCommLevel&cuid='.$cuid.'" method="post">
+  <label for="commDescription">Commission Level </label><br>
+  <input type="text" id="commDescription" name="commDescription" required value="'.$commDescription.'"><br>
+  <label for="commPercent">Commission Percentage:</label><br>
+  <input type="number" min="1" max="100" step="any" id="commPercent" name="commPercent" required value="'.$commPercent.'"><br><br>
   <input type="hidden" name="fromDB" value="no">
   <input type="hidden" name="wasiposted" value="yes">
   <input type="submit" value="Submit">
@@ -233,12 +231,12 @@ echo '
 
    if ($myerror=="" && $fromDB <> ""){
 
- 	$query = "UPDATE commission SET description='".addslashes($Commname)."',value='".addslashes($cpercentage)."' WHERE uid='".$cuid."'";
+ 	$query = "UPDATE commission SET commDescription='".addslashes($commDescription)."',value='".addslashes($commPercent)."' WHERE uid='".$cuid."'";
 	$result = mysqli_query($dbConn, $query);
 
     echo "Commission record updated successfully...";
 
-	echo '<meta http-equiv="refresh" content="0;url=commission.php?procedure=commission&showSnack='.$Commname.'- Record updated successfully..." />';
+	echo '<meta http-equiv="refresh" content="0;url=commission.php?procedure=commission&showSnack='.$commDescription.'- Record updated successfully..." />';
 
 	}
 
