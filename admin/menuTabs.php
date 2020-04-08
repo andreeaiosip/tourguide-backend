0e9f587ef02fca3cb07ae8522546f027d2226e25
Andreea Iosip <andrea.iosip@gmail.com>
2020-04-08 16:25:52 +0100

    Menu tab links updated

--- admin/menuTabs.php
+++ admin/menuTabs.php
@@ -1,8 +1,6 @@
 <?php
-/*
-if ($procedure=="home"){
-	 $myActivehome = "class='active'";
-	} */
+
+
 if ($procedure=="tours"){
 	 $myActiveTour = "class='active'";
 	}
@@ -21,20 +19,35 @@ if ($procedure=="payroll"){
         $myActivePayroll = "class='active'";
        }
 
-
+if ($procedure=="admin"){
+	 $myActiveAdmin = "class='active'";
+  } 
+  
+  if ($procedure=="logout"){
+    $myActiveLogout = "class='active'";
+   } 
 
 echo '<div class="navbar">';
-  //echo '<a '.  $myActivehome    . 'href="home.php?procedure=home"><i class="fa fa-fw fa-home"></i> HOME</a>';
+if ($_SESSION["AccessRole"]=="Adm"){
   echo '<a '.  $myActiveTour    . 'href="tours.php?procedure=tours"><i class="fa fa-fw fa-bus"></i> TOURS</a>';
   echo '<a '.  $myActiveGuide   . 'href="tourguides.php?procedure=tourguides"><i class="fa fa-user-circle-o" aria-hidden="true"></i> TOUR GUIDES</a>';
   echo '<a '.  $myActiveComms   . 'href="commission.php?procedure=commission"><i class="fa fa-handshake-o" aria-hidden="true"></i> COMM LEVEL</a>';
   echo '<a '.  $myActiveBook    . 'href="bookings.php?procedure=bookings"><i class="fa fa-book" aria-hidden="true"></i> BOOKINGS</a>';
-  echo '<a '.  $myActivePayroll . 'href="payroll.php?procedure=payroll"><i class="fa fa-money" aria-hidden="true"></i></i> PAYROLL</a>';
-  echo '<a '.  $myActiveAdmin   . 'href="plsremembertoaddme.php?procedure=admin"><i class="fa fa-fw fa-user"></i> ADMIN</a>';
-  echo '<a '.  $myActiveLogout  . 'href="index.php"><i class="fa fa-lock" aria-hidden="true"></i> LOGOUT</a>';
+  echo '<a '.  $myActivePayroll . 'href="myPayroll.php"><i class="fa fa-money" aria-hidden="true"></i></i> PAYROLL</a>';
+  echo '<a '.  $myActiveAdmin   . 'href="admin.php"><i class="fa fa-fw fa-user"></i> ADMIN</a>'
+  ;
+
+} else {
+  echo '<a '.  $myActiveBook    . 'href="bookings.php?procedure=bookings"><i class="fa fa-book" aria-hidden="true"></i> BOOKINGS</a>';
+ echo '<a '.  $myActivePayroll . 'href="myPayroll.php"><i class="fa fa-money" aria-hidden="true"></i></i> PAYROLL</a>';
+ echo "<span class='userAccess'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TG Logged in (".$SESSION["TG"].")</span>";
+}
+  
+echo '<a '.  $myActiveLogout  . 'href="/login.php"><i class="fa fa-lock" aria-hidden="true"></i> LOGOUT</a>';
 echo '</div><br>';
 //echo "<hr>";
 //echo '<div class="headerBar" align="right"><span class="label success"><i class="fa fa-map-signs" aria-hidden="true"></i> Your are here |  '.strtoupper($procedure).'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></div><br>';
 
+echo 'role:' .	$_SESSION["AccessRole"];
 
 ?>
